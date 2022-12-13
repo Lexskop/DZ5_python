d920a19091395589f1341856c52e4185a4e52da7
lexskop <lexskop@gmail.com>
2022-12-13 19:59:00 +0700

    Решена первая задачка

--- task1.py
+++ task1.py
@@ -2,3 +2,18 @@
 # Пример:
 # Пугать ты галок пугай => заданная строка "галок" => Пугать ты пугай
 # Пугать ты галок пугай => заданная строка "пуг" => Пугать ты галок
+
+input_data=input('Введите текст -> \n').split()
+input_word=input('Введите слово для удаления ->\n')
+
+def delete_selected_text(input_data:list,input_word:str) -> str:
+    """
+    Удаляет из принятого текста заданную строку
+    """
+    output_text=''
+    for i in range(len(input_data)):
+        if str(input_data[i]).find(input_word) == -1:
+            output_text+=input_data[i]+ ' '
+    return output_text
+
+print(delete_selected_text(input_data,input_word))
